00ec2d2c51085f6e36bb69a2912109a838e26ed5
costumebrother <costumebrother@f586cabc-7ac0-5a70-fe87-28ae3718d38a>
2011-09-15 01:23:10 +0000

    Add QOsgViewer and -widget. Prepare PropertySupport.

---
+++ apps/skybox/gui/propertysupport.cpp
@@ -0,0 +1,374 @@
+
+// Copyright (c) 2011, Daniel Müller <dm@g4t3.de>
+// Computer Graphics Systems Group at the Hasso-Plattner-Institute, Germany
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice, 
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright 
+//     notice, this list of conditions and the following disclaimer in the 
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Computer Graphics Systems Group at the 
+//     Hasso-Plattner-Institute (HPI), Germany nor the names of its 
+//     contributors may be used to endorse or promote products derived from 
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "propertysupport.h"
+
+#include <qtpropertybrowser/QtProperty>
+#include <qtpropertybrowser/QtAbstractPropertyBrowser>
+
+#include <qtpropertybrowser/QtGroupPropertyManager>
+#include <qtpropertybrowser/QtDoublePropertyManager>
+#include <qtpropertybrowser/QtSizeFPropertyManager>
+#include <qtpropertybrowser/QtBoolPropertyManager>
+#include <qtpropertybrowser/QtIntPropertyManager>
+#include <qtpropertybrowser/QtEnumPropertyManager>
+
+#include <qtpropertybrowser/QtEnumEditorFactory>
+#include <qtpropertybrowser/QtDoubleSpinBoxFactory>
+
+
+PropertySupport::PropertySupport()
+:   m_propertiesInitialized(false)
+,   m_groupManager(NULL)
+,   m_intManager(NULL)
+,   m_doubleManager(NULL)
+,   m_sizeFManager(NULL)
+,   m_boolManager(NULL)
+,   m_enumManager(NULL)
+,   m_enumFactory(NULL)
+,   m_boolFactory(NULL)
+,   m_intFactory(NULL)
+,   m_doubleFactory(NULL)
+,   m_sizeFactory(NULL)
+{
+}
+
+
+PropertySupport::~PropertySupport()
+{
+    delete m_groupManager;
+    delete m_intManager;
+    delete m_doubleManager;
+    delete m_sizeFManager;
+    delete m_boolManager;
+    delete m_enumManager;
+
+    delete m_enumFactory;
+    delete m_boolFactory;
+    delete m_intFactory;
+    delete m_doubleFactory;
+    delete m_sizeFactory;
+}
+
+
+
+QtProperty *PropertySupport::property(const QString &name) const
+{
+    assert(m_properties.contains(name));
+    return m_properties[name];
+}
+
+
+QtIntPropertyManager *PropertySupport::propertyIntManager() const
+{
+    return m_intManager;
+}
+
+
+QtDoublePropertyManager *PropertySupport::propertyDoubleManager() const
+{
+    return m_doubleManager;
+}
+
+
+QtBoolPropertyManager *PropertySupport::propertyBoolManager() const
+{
+    return m_boolManager;
+}
+
+
+QtProperty *PropertySupport::createGroup(const QString &groupName)
+{
+    assert(m_groupManager);
+
+    QtProperty *group = m_groupManager->addProperty(groupName);
+    m_rootGroups.append(group);
+
+    return group;
+}
+
+
+QtProperty *PropertySupport::createGroup(QtProperty &group, const QString &groupName)
+{
+    assert(m_groupManager);
+
+    QtProperty *subGroup = m_groupManager->addProperty(groupName);
+    group.addSubProperty(subGroup);
+
+    return subGroup;
+}
+
+
+QtProperty *PropertySupport::createProperty(QtProperty &group, const QString &propertyName, 
+	const int value, const QStringList &values)
+{
+    m_properties.insert(propertyName, m_enumManager->addProperty(propertyName));
+
+    QtProperty *p = property(propertyName);
+    m_enumManager->setEnumNames(p, values);
+    m_enumManager->setValue(p, value);
+
+    group.addSubProperty(p);
+
+    return p;
+}
+
+
+QtProperty *PropertySupport::createProperty(QtProperty &group, const QString &propertyName, 
+	const bool value)
+{
+    m_properties.insert(propertyName, m_boolManager->addProperty(propertyName));
+
+    QtProperty *p = property(propertyName);
+    m_boolManager->setValue(p, value);
+
+    group.addSubProperty(p);
+
+    return p;
+}
+
+
+QtProperty *PropertySupport::createProperty(QtProperty &group, const QString &propertyName, 
+	const double value, const double minValue, const double maxValue, const double stepValue)
+{
+    m_properties.insert(propertyName, m_doubleManager->addProperty(propertyName));
+
+    QtProperty *p = property(propertyName);
+    m_doubleManager->setValue(p, value);
+    m_doubleManager->setRange(p, minValue, maxValue);
+    m_doubleManager->setSingleStep(p, stepValue);
+
+    group.addSubProperty(p);
+
+    return p;
+}
+
+
+QtProperty *PropertySupport::createProperty(QtProperty &group, const QString &propertyName, 
+	const QSizeF &value, const QSizeF &minValue, const QSizeF &maxValue, const QSizeF &stepValue)
+{
+    m_properties.insert(propertyName, m_sizeFManager->addProperty(propertyName));
+
+    QtProperty *p = property(propertyName);
+    m_sizeFManager->setValue(p, value);
+    m_sizeFManager->setRange(p, minValue, maxValue);
+
+    m_sizeFManager->subDoublePropertyManager()->setSingleStep(p->subProperties()[0], stepValue.width());
+    m_sizeFManager->subDoublePropertyManager()->setSingleStep(p->subProperties()[1], stepValue.height());
+
+    group.addSubProperty(p);
+
+    return p;
+}
+
+
+QtProperty *PropertySupport::createProperty(QtProperty &group, const QString &propertyName, 
+	const int value, const int minValue, const int maxValue, const int stepValue)
+{
+    m_properties.insert(propertyName, m_intManager->addProperty(propertyName));
+
+    QtProperty *p = property(propertyName);
+    m_intManager->setValue(p, value);
+    m_intManager->setRange(p, minValue, maxValue);
+    m_intManager->setSingleStep(p, stepValue);
+
+    group.addSubProperty(p);
+
+    return p;
+}
+
+
+void PropertySupport::registerForFastAccess(const QString &name)
+{
+    m_fastAccessProperties.append(property(name));
+}
+
+
+void PropertySupport::initializeProperties()
+{
+    if(!m_propertiesInitialized)
+    {
+        m_propertiesInitialized = true;
+
+        m_groupManager = new QtGroupPropertyManager();
+        m_intManager = new QtIntPropertyManager();
+        m_doubleManager = new QtDoublePropertyManager();
+        m_sizeFManager = new QtSizeFPropertyManager();
+        m_boolManager = new QtBoolPropertyManager();
+        m_enumManager = new QtEnumPropertyManager();
+
+        m_enumFactory = new QtEnumEditorFactory();
+        m_boolFactory = new QtCheckBoxFactory();
+        m_intFactory = new QtSpinBoxFactory();
+        m_doubleFactory = new QtDoubleSpinBoxFactory();
+        m_sizeFactory = new QtDoubleSpinBoxFactory();
+
+        registerProperties();
+    }
+}
+
+
+void PropertySupport::floodPropertyBrowser(QtAbstractPropertyBrowser *propertyBrowser)
+{
+    assert(m_propertiesInitialized);
+
+    if(!propertyBrowser)
+        return;
+
+    connect(m_enumManager, SIGNAL(valueChanged(QtProperty*, int)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    connect(m_boolManager, SIGNAL(valueChanged(QtProperty*, bool)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    connect(m_intManager, SIGNAL(valueChanged(QtProperty*, int)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    connect(m_doubleManager, SIGNAL(valueChanged(QtProperty*, double)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    connect(m_sizeFManager, SIGNAL(valueChanged(QtProperty*, QSizeF)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+
+    propertyBrowser->setFactoryForManager(m_enumManager, m_enumFactory);
+    propertyBrowser->setFactoryForManager(m_boolManager, m_boolFactory);
+    propertyBrowser->setFactoryForManager(m_intManager, m_intFactory);
+    propertyBrowser->setFactoryForManager(m_doubleManager, m_doubleFactory);
+    propertyBrowser->setFactoryForManager(m_sizeFManager->subDoublePropertyManager(), m_sizeFactory);
+
+    // populate
+    foreach(QtProperty *p, m_rootGroups)
+        propertyBrowser->addProperty(p);
+}
+
+
+void PropertySupport::clearPropertyBrowser(QtAbstractPropertyBrowser *propertyBrowser)
+{
+    assert(m_propertiesInitialized);
+
+    if(!propertyBrowser)
+        return;
+
+    disconnect(m_enumManager, SIGNAL(valueChanged(QtProperty*, int)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    disconnect(m_boolManager, SIGNAL(valueChanged(QtProperty*, bool)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    disconnect(m_intManager, SIGNAL(valueChanged(QtProperty*, int)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    disconnect(m_doubleManager, SIGNAL(valueChanged(QtProperty*, double)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+    disconnect(m_sizeFManager, SIGNAL(valueChanged(QtProperty*, QSizeF)),
+        this, SLOT(on_propertyChanged(QtProperty*)));
+
+    propertyBrowser->unsetFactoryForManager(m_enumManager);
+    propertyBrowser->unsetFactoryForManager(m_boolManager);
+    propertyBrowser->unsetFactoryForManager(m_intManager);
+    propertyBrowser->unsetFactoryForManager(m_doubleManager);
+    propertyBrowser->unsetFactoryForManager(m_sizeFManager->subDoublePropertyManager());
+
+    propertyBrowser->clear();
+}
+
+
+const QList<QtProperty*> &PropertySupport::fastAccessProperties() const
+{
+    return m_fastAccessProperties;
+}
+
+
+void PropertySupport::on_propertyChanged(QtProperty *p)
+{
+    propertyChanged(p);
+}
+
+
+const double PropertySupport::doubleValue(const QString &name) const
+{
+    assert(m_properties.contains(name));
+    return m_doubleManager->value(m_properties[name]);
+}
+
+
+void PropertySupport::setDoubleValue(const QString &name, const double value)
+{
+    assert(m_properties.contains(name));
+    m_doubleManager->setValue(m_properties[name], value);
+}
+
+
+const int PropertySupport::intValue(const QString &name) const
+{
+    assert(m_properties.contains(name));
+    return m_intManager->value(m_properties[name]);
+}
+
+
+void PropertySupport::setIntValue(const QString &name, const int value)
+{
+    assert(m_properties.contains(name));
+    m_intManager->setValue(m_properties[name], value);
+}
+
+
+const int PropertySupport::enumValue(const QString &name) const
+{
+	assert(m_properties.contains(name));
+	return m_enumManager->value(m_properties[name]);
+}
+
+
+void PropertySupport::setEnumValue(const QString &name, const int value)
+{
+    assert(m_properties.contains(name));
+    m_enumManager->setValue(m_properties[name], value);
+}
+
+
+const bool PropertySupport::boolValue(const QString &name) const
+{
+    assert(m_properties.contains(name));
+    return m_boolManager->value(m_properties[name]);
+}
+
+
+void PropertySupport::setBoolValue(const QString &name, const bool value)
+{
+    assert(m_properties.contains(name));
+    m_boolManager->setValue(m_properties[name], value);
+}
+
+
+const QSizeF PropertySupport::sizeFValue(const QString &name) const
+{
+    assert(m_properties.contains(name));
+    return m_sizeFManager->value(m_properties[name]);
+}
+
+
+void PropertySupport::setSizeFValue(const QString &name, const QSizeF &value)
+{
+    assert(m_properties.contains(name));
+    m_sizeFManager->setValue(m_properties[name], value);
+}
---
+++ apps/skybox/gui/propertysupport.h
@@ -0,0 +1,146 @@
+
+// Copyright (c) 2011, Daniel Müller <dm@g4t3.de>
+// Computer Graphics Systems Group at the Hasso-Plattner-Institute, Germany
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice, 
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright 
+//     notice, this list of conditions and the following disclaimer in the 
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of the Computer Graphics Systems Group at the 
+//     Hasso-Plattner-Institute (HPI), Germany nor the names of its 
+//     contributors may be used to endorse or promote products derived from 
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+#ifndef __PROPERTYSUPPORT_H__
+#define __PROPERTYSUPPORT_H__
+
+
+#include "utils/abstractfactory.hpp"
+
+#include <QLabel>
+#include <QGraphicsItem>
+#include <QPainterPath>
+#include <QComboBox>
+
+class QtProperty;
+class QtAbstractPropertyBrowser;
+
+class QtGroupPropertyManager;
+class QtIntPropertyManager;
+class QtDoublePropertyManager;
+class QtSizeFPropertyManager;
+class QtBoolPropertyManager;
+class QtEnumPropertyManager;
+
+class QtEnumEditorFactory;
+class QtCheckBoxFactory;
+class QtSpinBoxFactory;
+class QtDoubleSpinBoxFactory;
+class QtDoubleSpinBoxFactory;
+
+class QSvgRenderer;
+
+
+class PropertySupport : public QObject
+{
+    Q_OBJECT
+
+public:
+    PropertySupport();
+    ~PropertySupport();
+
+    // properties
+    void floodPropertyBrowser(QtAbstractPropertyBrowser *propertyBrowser);
+    void clearPropertyBrowser(QtAbstractPropertyBrowser *propertyBrowser);
+
+    // special "fast-access" properties
+    const QList<QtProperty*> &fastAccessProperties() const;
+
+    QtIntPropertyManager *propertyIntManager() const;
+    QtDoublePropertyManager *propertyDoubleManager() const;
+    QtBoolPropertyManager *propertyBoolManager() const;
+
+protected:
+	// properties
+    bool m_propertiesInitialized;
+
+    QtGroupPropertyManager  *m_groupManager;
+
+    QtIntPropertyManager    *m_intManager;
+    QtDoublePropertyManager *m_doubleManager;
+    QtSizeFPropertyManager  *m_sizeFManager;
+    QtBoolPropertyManager   *m_boolManager;
+    QtEnumPropertyManager   *m_enumManager;
+
+    QtEnumEditorFactory     *m_enumFactory;
+    QtCheckBoxFactory       *m_boolFactory;
+    QtSpinBoxFactory        *m_intFactory;
+    QtDoubleSpinBoxFactory  *m_doubleFactory;
+    QtDoubleSpinBoxFactory  *m_sizeFactory;
+
+    QList<QtProperty*> m_rootGroups;
+    QHash<QString, QtProperty*> m_properties;
+
+    QList<QtProperty*> m_fastAccessProperties;
+
+
+protected slots:
+    void on_propertyChanged(QtProperty*);
+
+protected:
+    void initializeProperties();
+
+    virtual void propertyChanged(QtProperty *p) = 0;
+    virtual void registerProperties() = 0;
+
+    QtProperty *property(const QString &name) const;
+
+    // property stuff
+    QtProperty *createGroup(const QString &groupName);
+    QtProperty *createGroup(QtProperty &group, const QString &groupName);
+
+    QtProperty *createProperty(QtProperty &group, const QString &propertyName
+        , const int value, const QStringList &values);
+    QtProperty *createProperty(QtProperty &group, const QString &propertyName
+        , const bool value);
+    QtProperty *createProperty(QtProperty &group, const QString &propertyName
+        , const int value, const int minValue, const int maxValue, const int stepValue);
+    QtProperty *createProperty(QtProperty &group, const QString &propertyName
+        , const double value, const double minValue, const double maxValue, const double stepValue);
+    QtProperty *createProperty(QtProperty &group, const QString &propertyName 
+        , const QSizeF &value, const QSizeF &minValue, const QSizeF &maxValue, const QSizeF &stepValue);
+
+    void registerForFastAccess(const QString &name);
+
+    // property helper
+
+    const QSizeF sizeFValue(const QString &name) const;
+    void setSizeFValue(const QString &name, const QSizeF &value);
+    const double doubleValue(const QString &name) const;
+    void setDoubleValue(const QString &name, const double value);
+    const int intValue(const QString &name) const;
+    void setIntValue(const QString &name, const int value);
+    const bool boolValue(const QString &name) const;
+    void setBoolValue(const QString &name, const bool value);
+    const int enumValue(const QString &name) const;
+    void setEnumValue(const QString &name, const int value);
+};
+
+#endif __PROPERTYSUPPORT_H__
