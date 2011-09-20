b9db5f61a6024b3fef022b7e465c8c7d3b70cc46
costumebrother <costumebrother@f586cabc-7ac0-5a70-fe87-28ae3718d38a>
2011-09-20 22:53:47 +0000

    Add further useful interpolation methods and allow direct use of macros.

--- include/interpolate.h
+++ include/interpolate.h
@@ -31,12 +31,40 @@
 #ifndef __INTERPOLATE_H__
 #define __INTERPOLATE_H__
 
+#include <math.h>
+
+
+#define interpolate_linear(t)       (t)
+
+#define interpolate_smoothstep(t)   (t * t * (3 - 2 * t))
+#define interpolate_smoothstep2(t)  (interpolate_smoothstep(interpolate_smoothstep(t)))
+#define interpolate_smoothstep3(t)  (interpolate_smoothstep(interpolate_smoothstep2(t)))
+
+#define interpolate_smootherstep(t) (t * t * t * (t * (6 * t - 15) + 10))
+
+#define interpolate_squared(t)      (t * t)
+#define interpolate_invsquared(t)   (1 - (1 - t) * (1 - t))
+
+#define interpolate_cubed(t)        (t * t * t)
+#define interpolate_invcubed(t)     (1 - (1 - t) * (1 - t) * (1 - t))
+
+#define interpolate_sin(t)          (sin(t * 1.57079632679489661923))
+#define interpolate_invsin(t)       (1 - sin((1 - t) * 1.57079632679489661923))
+
 
 enum e_InterpolationMethod
 {
     IM_LINEAR
 ,   IM_SMOOTHSTEP
+,   IM_SMOOTHSTEP2
+,   IM_SMOOTHSTEP3
 ,   IM_SMOOTHERSTEP // Ken Perlin
+,   IM_SQUARED
+,   IM_INVSQUARED
+,   IM_CUBED
+,   IM_INVCUBED
+,   IM_SIN          // strong out, soft ending
+,   IM_INVSIN       // soft out, strong ending
 };
 
 
@@ -48,12 +76,28 @@ inline const T interpolate(
     switch(function)
     {
     case IM_SMOOTHSTEP:
-        return 3 * t * t - 2 * t * t * t;
+        return interpolate_smoothstep(t);
+    case IM_SMOOTHSTEP2:
+        return interpolate_smoothstep2(t);
+    case IM_SMOOTHSTEP3:
+        return interpolate_smoothstep3(t);
     case IM_SMOOTHERSTEP:
-        return 6 * t * t * t * t * t - 15 * t * t * t * t + 10 * t * t * t;
+        return interpolate_smootherstep(t);
+    case IM_SQUARED:
+        return interpolate_squared(t);
+    case IM_INVSQUARED:
+        return interpolate_invsquared(t);
+    case IM_CUBED:
+        return interpolate_cubed(t);
+    case IM_INVCUBED:
+        return interpolate_invcubed(t);
+    case IM_SIN:
+        return static_cast<T>(interpolate_sin(t));
+    case IM_INVSIN:
+        return static_cast<T>(interpolate_invsin(t));
     default:
     case IM_LINEAR:
-        return t;
+        return interpolate_linear(t);
     }
 }
 
