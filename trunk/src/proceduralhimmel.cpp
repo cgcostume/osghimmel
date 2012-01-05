292ed0251230c7874edfde6b79534f1c1a73185d
costumebrother <costumebrother@f586cabc-7ac0-5a70-fe87-28ae3718d38a>
2012-01-05 17:02:14 +0000

    Add Moon Hack.

--- trunk/src/proceduralhimmel.cpp
+++ trunk/src/proceduralhimmel.cpp
@@ -52,6 +52,8 @@ ProceduralHimmel::ProceduralHimmel()
     getOrCreateStateSet()->addUniform(u_ditheringMultiplier);
 
     addChild(m_hquad);
+
+    moon_hack();
 };
 
 
@@ -113,6 +115,174 @@ const float ProceduralHimmel::getDitheringMultiplier() const
     return multiplier;
 }
 
+
+#include "shadermodifier.h"
+#include <osg/TextureCubeMap>
+#include <osg/Texture2D>
+
+#include <osgDB/ReadFile>
+
+#include <osg/Geode>
+#include <osg/Geometry>
+
+
+void ProceduralHimmel::moon_hack()
+{
+// - Hack that transforms a quad to the moons position into the canopy.
+// - Generates circle with sphere normals and adds normals from moon cube map
+// - Applies lighting from sun - moon phase is correct (no extra calc for that ;))
+// - TODO: BRDF of Moon
+// - TODO: Correct Moon rotation (Face towards earth is still wrong)
+
+#pragma NOTE("Beautify this!")
+
+    m_mquad = new HimmelQuad();
+
+    addChild(m_mquad);
+
+    m_mprogram = new osg::Program;
+    m_mvShader = new osg::Shader(osg::Shader::VERTEX);
+    m_mfShader = new osg::Shader(osg::Shader::FRAGMENT);
+ //   m_mgShader = new osg::Shader(osg::Shader::GEOMETRY);
+
+    osg::StateSet* stateSet = m_mquad->getOrCreateStateSet();
+
+    setupNode(stateSet);
+ 
+    stateSet->addUniform(u_moon);
+    stateSet->addUniform(u_sun);
+
+    u_mcube = new osg::Uniform("mooncube", 0);
+    stateSet->addUniform(u_mcube);
+
+    //m_mprogram->addShader(m_mgShader);
+    m_mprogram->addShader(m_mvShader);
+    m_mprogram->addShader(m_mfShader);
+
+    stateSet->setAttributeAndModes(m_mprogram, osg::StateAttribute::ON);
+
+
+    osg::ref_ptr<osg::TextureCubeMap> tcm(new osg::TextureCubeMap);
+
+    tcm->setUnRefImageDataAfterApply(true);
+
+    tcm->setInternalFormat(GL_RGBA);
+
+    tcm->setWrap(osg::Texture::WRAP_S, osg::Texture::CLAMP_TO_EDGE);
+    tcm->setWrap(osg::Texture::WRAP_T, osg::Texture::CLAMP_TO_EDGE);
+    tcm->setWrap(osg::Texture::WRAP_R, osg::Texture::CLAMP_TO_EDGE);
+
+    tcm->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR_MIPMAP_LINEAR);
+    tcm->setFilter(osg::Texture::MAG_FILTER, osg::Texture::LINEAR);
+
+    tcm->setImage(osg::TextureCubeMap::POSITIVE_X, osgDB::readImageFile("resources/moon_px.png"));
+    tcm->setImage(osg::TextureCubeMap::NEGATIVE_X, osgDB::readImageFile("resources/moon_nx.png"));
+    tcm->setImage(osg::TextureCubeMap::POSITIVE_Y, osgDB::readImageFile("resources/moon_py.png"));
+    tcm->setImage(osg::TextureCubeMap::NEGATIVE_Y, osgDB::readImageFile("resources/moon_ny.png"));
+    tcm->setImage(osg::TextureCubeMap::POSITIVE_Z, osgDB::readImageFile("resources/moon_pz.png"));
+    tcm->setImage(osg::TextureCubeMap::NEGATIVE_Z, osgDB::readImageFile("resources/moon_nz.png"));
+
+    stateSet->setTextureAttributeAndModes(0, tcm, osg::StateAttribute::ON);
+
+
+ //   m_mgShader->loadShaderSourceFromFile("D:/p/osghimmel/tempg.txt");
+    m_mvShader->loadShaderSourceFromFile("D:/p/osghimmel/tempv.txt");
+    m_mfShader->loadShaderSourceFromFile("D:/p/osghimmel/tempf.txt");
+    
+
+/* VERTEX
+
+#version 150 compatibility
+
+uniform vec3 moon;
+
+out mat3 test;
+
+void main(void)
+{
+    vec3 m = normalize(moon);
+
+	vec3 u = normalize(cross(m.xyz, vec3(1)));
+	vec3 v = normalize(cross(u, m.xyz));
+
+	test = mat3(u, v, m);
+
+    vec3 f = m - (gl_Vertex.x * u + gl_Vertex.y * v) * 0.018; // 0.018; // yields a 2° moon
+
+	gl_TexCoord[0] = gl_Vertex;
+    gl_Position = gl_ModelViewProjectionMatrix * vec4(f, 1.0);
+}
+
+*/
+
+
+/* FRAGMENT
+
+#version 150 compatibility
+
+uniform vec3 moon; // expected to be normalized
+uniform vec3 sun; // expected to be normalized
+
+uniform samplerCube moonCube;
+
+const float radius = 0.98;
+
+in vec4 m_eye;
+
+in mat3 test;
+
+void main(void)
+{
+	float x = gl_TexCoord[0].x;
+	float y = gl_TexCoord[0].y;
+
+	float zz = radius * radius - x * x - y * y;
+			
+	if(zz < 0.02)
+		discard;
+
+	float z = sqrt(zz);
+
+	mat3 m = test;;
+
+	// optimize later to mt = mn.zyx and mb = mn.xzy ;)
+	vec3 mn = normalize(m * vec3(x, y, z));
+	vec3 mt = mn.zyx;
+	vec3 mb = mn.xzy;
+
+	vec4  c  = textureCube(moonCube, -mn);
+	vec3  cn = normalize((c.xyz) * 2.0 - 1.0);
+
+	float cd = c.a;
+
+	vec3 n = vec3(dot(cn, mt), dot(cn, mb), dot(cn, mn));
+//	vec3 d = vec3(dot(-sun, n)) * cd;
+	vec3 d = 2.0 * vec3(0.1 + smoothstep(-0.2, 0.2, dot(-sun, n))) * cd;
+
+	gl_FragColor = vec4(d * vec3(1.06, 1.06, 0.98), 1.0);
+
+	// debug
+
+//	gl_FragColor = vec4(x * 0.5 + 0.5, y * 0.5 + 0.5, 0.0, 0.0);
+//	gl_FragColor = vec4(mn * 0.5 + 0.5, 1.0);
+//	gl_FragColor = vec4(n * 0.5 + 0.5, 1.0);
+//	gl_FragColor = vec4(vec3(cd), 1.0);
+//	gl_FragColor = vec4(d * vec3(1.06, 1.06, 0.98), 1.0);
+}
+
+*/
+
+
+
+#ifdef OSGHIMMEL_ENABLE_SHADERMODIFIER
+    if(shaderModifier())
+    {
+        shaderModifier()->registerShader("moon_hack", m_mfShader);
+        shaderModifier()->registerShader("moon_hack", m_mvShader);
+        //shaderModifier()->registerShader("moon_hack", m_mgShader);
+    }
+#endif // OSGHIMMEL_ENABLE_SHADERMODIFIER
+}
 
 
 
