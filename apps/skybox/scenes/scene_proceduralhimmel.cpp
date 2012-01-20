e582b717f2faf4a017e316a7ee711bce8315e0fd
costumebrother <costumebrother@f586cabc-7ac0-5a70-fe87-28ae3718d38a>
2012-01-20 08:48:20 +0000

    Add properties for stars and moon geodes in skybox. - Add color conversion from/to osg::Vec3 to/from QColor.

--- apps/skybox/scenes/scene_proceduralhimmel.cpp
+++ apps/skybox/scenes/scene_proceduralhimmel.cpp
@@ -32,6 +32,10 @@
 #include "include/proceduralhimmel.h"
 #include "include/atmospheregeode.h"
 #include "include/moongeode.h"
+#include "include/starsgeode.h"
+#include "include/abstractastronomy.h"
+
+#include "utils/qt2osg.h"
 
 
 namespace
@@ -40,8 +44,27 @@ namespace
 
     static const QString GROUP_PROCEDURAL(TR("Procedrual"));
 
+    static const QString GROUP_PROCEDURAL_ATMOSPHERE(TR("Atmosphere"));
     static const QString PROPERTY_DITHERING_MULTIPLIER (TR("Dithering Multiplier"));
-    static const QString PROPERTY_MOON_SCALE           (TR("Moon Scale"));
+    
+    static const QString GROUP_PROCEDURAL_MOON(TR("Moon"));
+    static const QString PROPERTY_MOON_SCALE               (TR("Scale"));
+    static const QString PROPERTY_MOON_SUNSHINE_COLOR      (TR("Sun Shine Color"));
+    static const QString PROPERTY_MOON_SUNSHINE_INTENSITY  (TR("Sun Shine Intensity"));
+    static const QString PROPERTY_MOON_EARTHSHINE_COLOR    (TR("Earth Shine Color"));
+    static const QString PROPERTY_MOON_EARTHSHINE_INTENSITY(TR("Earth Shine Intensity"));
+    static const QString PROPERTY_MOON_POSITION_OVERRIDE   (TR("Moon Position Override"));
+    static const QString PROPERTY_MOON_POSITION_X          (TR("Moon Position X"));
+    static const QString PROPERTY_MOON_POSITION_Y          (TR("Moon Position Y"));
+    static const QString PROPERTY_MOON_POSITION_Z          (TR("Moon Position Z"));
+
+    static const QString GROUP_PROCEDURAL_STARS(TR("Stars"));
+
+    static const QString PROPERTY_STARS_GLARE_INTENSITY(TR("Glare Intensity"));
+    static const QString PROPERTY_STARS_GLARE_SCALE    (TR("Glare Scale"));
+    static const QString PROPERTY_STARS_MAX_VMAG       (TR("Max Visible Magnitude"));
+
+    static const QString GROUP_PROCEDURAL_GALAXY(TR("Galaxy"));
 }
 
 Scene_ProceduralHimmel::Scene_ProceduralHimmel(osg::Camera *camera)
@@ -73,7 +96,24 @@ void Scene_ProceduralHimmel::registerProperties()
     QtProperty *proceduralGroup = createGroup(GROUP_PROCEDURAL);
 
     createProperty(*proceduralGroup, PROPERTY_DITHERING_MULTIPLIER, AtmosphereGeode::defaultDitheringMultiplier(), 0.0, 255.0, 1.00);
-    createProperty(*proceduralGroup, PROPERTY_MOON_SCALE, MoonGeode::defaultScale(), 0.0, 100.0, 0.1); 
+
+    QtProperty *moonGroup = createGroup(GROUP_PROCEDURAL_MOON);
+
+    createProperty(*moonGroup, PROPERTY_MOON_SCALE, MoonGeode::defaultScale(), 0.0, 100.0, 0.25); 
+    createProperty(*moonGroup, PROPERTY_MOON_SUNSHINE_COLOR, toQColor(MoonGeode::defaultSunShineColor())); 
+    createProperty(*moonGroup, PROPERTY_MOON_SUNSHINE_INTENSITY, MoonGeode::defaultSunShineIntensity(), 0.0, 100.0, 0.25); 
+    createProperty(*moonGroup, PROPERTY_MOON_EARTHSHINE_COLOR, toQColor(MoonGeode::defaultEarthShineColor())); 
+    createProperty(*moonGroup, PROPERTY_MOON_EARTHSHINE_INTENSITY, MoonGeode::defaultEarthShineIntensity(), 0.0, 100.0, 0.25); 
+    createProperty(*moonGroup, PROPERTY_MOON_POSITION_OVERRIDE, false); 
+    createProperty(*moonGroup, PROPERTY_MOON_POSITION_X, 0.0, -1.0, 1.0, 0.01); 
+    createProperty(*moonGroup, PROPERTY_MOON_POSITION_Y, 0.0, -1.0, 1.0, 0.01); 
+    createProperty(*moonGroup, PROPERTY_MOON_POSITION_Z, 0.0, -1.0, 1.0, 0.01);   
+
+    QtProperty *starGroup = createGroup(GROUP_PROCEDURAL_STARS);
+
+    createProperty(*starGroup, PROPERTY_STARS_GLARE_INTENSITY, 1.0, 0.0, 100.0, 0.1); 
+    createProperty(*starGroup, PROPERTY_STARS_GLARE_SCALE, 1.0, 0.0, 100.0, 0.1); 
+    createProperty(*starGroup, PROPERTY_STARS_MAX_VMAG, StarsGeode::defaultMaxVMag(), -32.0, 32.0, 0.1); 
 }
 
 
@@ -86,6 +126,37 @@ void Scene_ProceduralHimmel::propertyChanged(
     if(PROPERTY_MOON_SCALE == name)
         m_himmel->moon()->setScale(doubleValue(PROPERTY_MOON_SCALE));
 
+    if(PROPERTY_MOON_SUNSHINE_COLOR == name)
+        m_himmel->moon()->setSunShineColor(toVec3(colorValue(PROPERTY_MOON_SUNSHINE_COLOR)));
+    if(PROPERTY_MOON_SUNSHINE_INTENSITY == name)
+        m_himmel->moon()->setSunShineIntensity(doubleValue(PROPERTY_MOON_SUNSHINE_INTENSITY));
+
+    if(PROPERTY_MOON_EARTHSHINE_COLOR == name)
+        m_himmel->moon()->setEarthShineColor(toVec3(colorValue(PROPERTY_MOON_EARTHSHINE_COLOR)));
+    if(PROPERTY_MOON_EARTHSHINE_INTENSITY == name)
+        m_himmel->moon()->setEarthShineIntensity(doubleValue(PROPERTY_MOON_EARTHSHINE_INTENSITY));
+
+    if(PROPERTY_MOON_POSITION_OVERRIDE == name)
+        m_himmel->astro()->setOverrideMoonPosition(boolValue(PROPERTY_MOON_POSITION_OVERRIDE));
+    
+    if(PROPERTY_MOON_POSITION_X == name || PROPERTY_MOON_POSITION_Y == name || PROPERTY_MOON_POSITION_Z == name)
+    {
+        osg::Vec3 vec;
+        vec._v[0] =  doubleValue(PROPERTY_MOON_POSITION_X);
+        vec._v[1] =  doubleValue(PROPERTY_MOON_POSITION_Y);
+        vec._v[2] =  doubleValue(PROPERTY_MOON_POSITION_Z);
+        vec.normalize();
+        m_himmel->astro()->setMoonPosition(vec);
+    }
+
+    if(PROPERTY_STARS_GLARE_INTENSITY == name)
+        m_himmel->stars()->setGlareIntensity(doubleValue(PROPERTY_STARS_GLARE_INTENSITY));
+
+    if(PROPERTY_STARS_GLARE_SCALE == name)
+        m_himmel->stars()->setGlareScale(doubleValue(PROPERTY_STARS_GLARE_SCALE));
+
+    if(PROPERTY_STARS_MAX_VMAG == name)
+        m_himmel->stars()->setMaxVMag(doubleValue(PROPERTY_STARS_MAX_VMAG));
 }
 
 
