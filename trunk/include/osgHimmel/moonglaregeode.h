4725a0e32c1d3eb606f4d1ee128e1a2efc0cbace
costumebrother <costumebrother@f586cabc-7ac0-5a70-fe87-28ae3718d38a>
2012-04-23 13:43:26 +0000

    Add moon glare geode. - Move per-frame-static shader code to CPU. - Reuse moongeodes' uniforms.

--- trunk/include/osgHimmel/moonglaregeode.h
+++ trunk/include/osgHimmel/moonglaregeode.h
@@ -28,8 +28,8 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
-#ifndef __MOONGEODE_H__
-#define __MOONGEODE_H__
+#ifndef __MOONGLAREGEODE_H__
+#define __MOONGLAREGEODE_H__
 
 #include "declspec.h"
 
@@ -43,46 +43,29 @@ class Himmel;
 class HimmelQuad;
 
 
-class OSGH_API MoonGeode : public osg::Geode
+class OSGH_API MoonGlareGeode : public osg::Geode
 {
 public:
 
-    MoonGeode(const char* cubeMapFilePath);
-    virtual ~MoonGeode();
+    MoonGlareGeode();
+    virtual ~MoonGlareGeode();
 
     void update(const Himmel &himmel);
 
-    // This is used to add this instances uniforms to e.g. moons glare geode.
-    void addUniformsToVariousStateSate(osg::StateSet* stateSet);
-
+    // scale is in n times the size of the moon
     const float setScale(const float scale);
     const float getScale() const;
     static const float defaultScale();
 
-    const osg::Vec3 setSunShineColor(const osg::Vec3 &color);
-    const osg::Vec3 getSunShineColor() const;
-    static const osg::Vec3 defaultSunShineColor();
-
-    const float setSunShineIntensity(const float intensity);
-    const float getSunShineIntensity() const;
-    static const float defaultSunShineIntensity();
-
-    const osg::Vec3 setEarthShineColor(const osg::Vec3 &color);
-    const osg::Vec3 getEarthShineColor() const;
-    static const osg::Vec3 defaultEarthShineColor();
-
-    const float setEarthShineIntensity(const float intensity);
-    const float getEarthShineIntensity() const;
-    static const float defaultEarthShineIntensity();
+    // intensity is in fraction of moons intensity
+    const float setIntensity(const float intensity);
+    const float getIntensity() const;
+    static const float defaultIntensity();
 
 protected:
 
     void setupUniforms(osg::StateSet* stateSet);
     void setupNode    (osg::StateSet* stateSet);
-
-    void setupTextures(
-        osg::StateSet* stateSet
-    ,   const char* cubeMapFilePath);
 
     void setupShader  (osg::StateSet* stateSet);
 
@@ -97,18 +80,9 @@ protected:
     osg::Shader *m_vShader;
     osg::Shader *m_fShader;
 
-    osg::ref_ptr<osg::Uniform> u_moon;
-
-    osg::ref_ptr<osg::Uniform> u_moonCube;
-    osg::ref_ptr<osg::Uniform> u_R;
-    osg::ref_ptr<osg::Uniform> u_sunShine;
-    osg::ref_ptr<osg::Uniform> u_earthShine;
-
-    float m_scale;
-
-    float m_earthShineScale;
-    osg::Vec3 m_earthShineColor;
-
+    osg::ref_ptr<osg::Uniform> u_scale;
+    osg::ref_ptr<osg::Uniform> u_intensity;
+    osg::ref_ptr<osg::Uniform> u_phase;
 
 #ifdef OSGHIMMEL_EXPOSE_SHADERS
 public:
@@ -120,4 +94,4 @@ public:
 
 } // namespace osgHimmel
 
-#endif // __MOONGEODE_H__
+#endif // __MOONGLAREGEODE_H__
